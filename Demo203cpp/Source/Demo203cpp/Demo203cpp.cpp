7eecb12f41308dc9e81a98024b9ed9d545f2739e
HanYu1983 <hanyushome@gmail.com>
2018-11-13 16:27:12 +0800

    no message

---
+++ Demo203cpp/Source/Demo203cpp/Demo203cpp.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Demo203cpp.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Demo203cpp, "Demo203cpp" );
---
+++ Demo203cpp/Source/Demo203cpp/Demo203cppGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Demo203cppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMO203CPP_API ADemo203cppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
